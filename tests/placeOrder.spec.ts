c0b6211d0017c0d1e47d742dd21ef6ff36713b80
SkyJokeAlex <skyjoke@gmail.com>
2024-05-12 09:49:03 +0300

    Change test name

--- tests/placeOrder.spec.ts
+++ tests/placeOrder.spec.ts
@@ -5,7 +5,7 @@ import { TradeDialog } from '../PageObject/Pages/Dialogs/TradeDialog';
 import { TradeWatchPanel } from '../PageObject/Pages/Panels/TradeWatchPanel';
 
 
-test('Positions counter shows Placed Order ', async ({ page }) => {
+test('Positions counter increments after order was placed', async ({ page }) => {
   const ctraderPage = new BaseCtraderPage(page);
   const logInDialog = new LogInDialog(page);
   const tradeDialog = new TradeDialog(page);
